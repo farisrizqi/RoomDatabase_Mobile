ab73b88c3468949608feb3b74cc4beabb40a889a
farisrizqi <farisnur07@gmail.com>
2025-01-08 07:39:09 +0700

    Learn SQL Lite Mobile

---
+++ app/src/main/java/com/example/pertemuan4_ges/Barang.kt
@@ -0,0 +1,19 @@
+package com.example.pertemuan4_ges
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Barang(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+    @ColumnInfo(name = "nama")
+    var nama: String? = null,
+    @ColumnInfo(name = "jenis")
+    var jenis: String? = null,
+    @ColumnInfo(name = "harga")
+    var harga: Int? = null,
+)
+
